0c77bf06a1c5edf5025c4caa09c21621949512eb
Hill <hillonyechekwa@gmail.com>
2025-02-11 14:24:38 +0100

    Refactor sign-out route by removing unused import and simplifying GET function

--- app/api/auth/signout/route.ts
+++ app/api/auth/signout/route.ts
@@ -3,9 +3,9 @@ import { deleteSession } from "@/utils/session";
 import { BACKEND_URL } from "@/utils/constants";
 import { redirect, RedirectType } from "next/navigation";
 
-import { NextRequest } from "next/server";
+// import { NextRequest } from "next/server";
 
-export async function GET(req: NextRequest) {
+export async function GET() {
     const response = await authFetch(`${BACKEND_URL}/auth/signout`, {
         method: "POST"
     })
