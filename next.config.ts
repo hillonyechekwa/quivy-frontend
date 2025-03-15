df2fab1a9ad6af8614f40c6ae61873ce32700519
Hill <hillonyechekwa@gmail.com>
2025-03-15 21:02:42 +0100

    Add serverRuntimeConfig to next.config.ts to set the server port

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverRuntimeConfig: {
+    port: process.env.PORT || 3000
+  }
 };
 
 export default nextConfig;
